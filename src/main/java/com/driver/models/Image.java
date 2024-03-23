23a75b30aa470967a79aad820b476e75fb4defcd
Sooryamol-S <sooryamolsivapuri@gmail.com>
2024-03-23 21:38:37 +0530

    Che

--- src/main/java/com/driver/models/Image.java
+++ src/main/java/com/driver/models/Image.java
@@ -12,27 +12,27 @@ public class Image {
     @JoinColumn
     @ManyToOne
     private Blog blog;
-    private String dimension;
+    private String dimensions;
     private String description;
 
-    public Image( Blog blog, String dimension,String description) {
+    public Image( Blog blog, String dimensions,String description) {
         this.blog = blog;
-        this.dimension = dimension;
+        this.dimensions = dimensions;
         this.description=description;
     }
 
     public Image() {
         this.blog=null;
-        this.dimension=null;
+        this.dimensions=null;
         this.description=null;
     }
 
-    public String getDimension() {
-        return dimension;
+    public String getDimensions() {
+        return dimensions;
     }
 
-    public void setDimension(String dimension) {
-        this.dimension = dimension;
+    public void setDimensions(String dimensions) {
+        this.dimensions= dimensions;
     }
 
     public Integer getId() {
